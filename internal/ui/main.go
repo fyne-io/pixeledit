a9805e50fec8eb72836064f520ea55886454ea8d
Andy Williams <andy@andywilliams.me>
2020-07-23 15:18:38 +0100

    Add open option so we can work from installed app at last

--- internal/ui/main.go
+++ internal/ui/main.go
@@ -4,6 +4,7 @@ import (
 	"fyne.io/fyne"
 	"fyne.io/fyne/dialog"
 	"fyne.io/fyne/layout"
+	"fyne.io/fyne/storage"
 	"fyne.io/fyne/theme"
 	"fyne.io/fyne/widget"
 
@@ -14,8 +15,22 @@ type toolbar struct {
 	edit api.Editor
 }
 
-func (t *toolbar) toolbarSave() {
-	t.edit.Save()
+func (t *toolbar) toolbarOpen() {
+	win := fyne.CurrentApp().Driver().AllWindows()[0]
+	open := dialog.NewFileOpen(func(read fyne.URIReadCloser, err error) {
+		if err != nil {
+			dialog.ShowError(err, win)
+			return
+		}
+		if read == nil {
+			return
+		}
+
+		t.edit.LoadFile(read)
+	}, win)
+
+	open.SetFilter(storage.NewExtensionFileFilter([]string{".png"}))
+	open.Show()
 }
 
 func (t *toolbar) toolbarReset() {
@@ -30,10 +45,15 @@ func (t *toolbar) toolbarReset() {
 		}, win)
 }
 
+func (t *toolbar) toolbarSave() {
+	t.edit.Save()
+}
+
 func buildToolbar(e api.Editor) fyne.CanvasObject {
 	t := &toolbar{edit: e}
 
 	return widget.NewToolbar(
+		&widget.ToolbarAction{Icon: theme.FolderOpenIcon(), OnActivated: t.toolbarOpen},
 		&widget.ToolbarAction{Icon: theme.CancelIcon(), OnActivated: t.toolbarReset},
 		&widget.ToolbarAction{Icon: theme.DocumentSaveIcon(), OnActivated: t.toolbarSave},
 	)
